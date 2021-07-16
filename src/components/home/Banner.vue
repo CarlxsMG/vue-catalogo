7c3951a338a059c72c0d089ef6d478a11b2ff68b
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-07-17 00:04:38 +0200

    Home created

---
+++ src/components/home/Banner.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="banner">
+        <img src="../../assets/banner.png" class="banner__img">
+
+        <h1 class="banne__title">Bienvenido a mi tienda de practicas</h1>
+
+        <div class="banner__copy">
+            <p class="banner__copy__text">Es basicamente un catalogo</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Banner'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .banner {
+        border: 1px solid;
+        &__img {
+            width: 100%;
+            height: 200px;
+        }
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Vista Home</h1>
+    <banner />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import Banner from '../components/home/Banner.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Banner
   }
 }
 </script>
