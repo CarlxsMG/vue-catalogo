e4fd4a4a0f0457e74ba43eaa4ce158a891b67690
Carlos Martinez <carlosmartiguerre@gmail.com>
2021-07-18 11:07:50 +0200

    Banner design

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -2,7 +2,7 @@
     <div class="banner">
         <img src="../../assets/banner.png" class="banner__img">
 
-        <h1 class="banne__title">Bienvenido a mi tienda de practicas</h1>
+        <h1 class="banner__title">Bienvenido a mi tienda de practicas</h1>
 
         <div class="banner__copy">
             <p class="banner__copy__text">Es basicamente un catalogo</p>
@@ -20,12 +20,28 @@
     .banner {
         border: 1px solid red;
         text-align: center;
+
         &__img {
             width: 100%;
             height: 200px;
-            @include desde($medium) {
+            @include desde($mediumScreen) {
                 height: 400px;
             }
+        }
+
+        &__title {
+            font-family: $fontTitulo;
+            color: $colorFirst;
+        }
+
+        &__copy {
+            padding: 1em;
+            background: $colorThird;
+
+            &__text {
+                font-size: 12px;
+                font-family: $fontTextos;
+        }
         }
     }
 </style>
